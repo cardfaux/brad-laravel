a3497e31ebafa5eab9da34851a14fc736bb3a914
James Hagood <cardfaux@tutanota.com>
2023-08-20 12:59:17 -0500

    set up an example controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Example\ExampleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ExampleController::class, 'HomePage']);
+
+Route::get('/about', [ExampleController::class, 'AboutPage']);
