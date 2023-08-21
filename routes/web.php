e85ea298d226700f7958b0763a518e4c803d4bcb
James Hagood <cardfaux@tutanota.com>
2023-08-21 15:25:10 -0500

    added a custom middleware

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ use App\Http\Controllers\User\UserController;
 
 //* User related routes
 Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
-Route::post('/register', [UserController::class, 'register'])->middleware('guest');
-Route::post('/login', [UserController::class, 'login'])->middleware('guest');
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+Route::post('/register', [UserController::class, 'register'])->middleware('guest')->name('register');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest')->name('login.page');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn')->name('logout');
 
 //* Blog post related routes
-Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('auth')->name('create-post');
-Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('auth')->name('store-new-post');
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn')->name('create-post');
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn')->name('store-new-post');
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost'])->name('view-single-post');
