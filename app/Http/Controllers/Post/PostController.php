84e58280a1c839f445f784c11baa51ba5250689e
James Hagood <cardfaux@tutanota.com>
2023-08-27 19:30:54 -0500

    set up a few api endpoints

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -56,6 +56,24 @@ class PostController extends Controller
         return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created.');
     }
 
+    public function storeNewPostApi(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        $newPost = Post::create($incomingFields);
+
+        dispatch(new SendNewPostEmail(['sendTo' => auth()->user()->email, 'name' => auth()->user()->username, 'title' => $newPost->title]));
+
+        return $newPost->id;
+    }
+
     /**
      * Display the specified resource.
      */
@@ -100,5 +118,11 @@ class PostController extends Controller
         $post->delete();
 
         return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted.');
+    }
+
+    public function deleteApi(Post $post)
+    {
+        $post->delete();
+        return 'true';
     }
 }
